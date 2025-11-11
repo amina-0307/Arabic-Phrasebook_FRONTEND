88c0b2d1217cae4f7406d04efa41dac066697be8
Amina Saleem <amina_0307@hotmail.com>
2025-11-11 15:00:59 +0000

    fix: remove leftover counter.js import and clean entry files

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
-import './style.css';
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { setupCounter } from './counter.js';
 import App from "./App.jsx";
+import "./style.css";
 
-createRoot(document.getElementById("root")).render(<App />);
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
